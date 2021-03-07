225ce3f2ba6e84e48342f5e3618ad4f19edeed0c
Alejandro Maure <alejandrodanielmaure@gmail.com>
2021-03-07 17:54:10 -0300

    Ejercicio objetos

---
+++ Proyecto Simulador Interactivo/assets/js/data.js
@@ -0,0 +1,136 @@
+//objetos
+
+//proplan
+let proplanPerro = {
+    marca: "Proplan",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//eukanuba
+let eukanuba = {
+    marca: "Eukanuba",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//vital can
+let vitalCan = {
+    marca: "Vital Can",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//royal canin
+let royalCanin = {
+    marca: "Royal Canin",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//excellent
+let Excellent = {
+    marca: "Excellent",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+///dog chow
+let dogChow = {
+    marca: "Dog Chow",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//Dogui
+let dogui = {
+    marca: "Dogui",
+    animal: "Perro",
+    edad: "",
+    tamano: "",
+
+}
+
+//raza
+let raza = {
+    marca: "Raza",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//sabrosito
+let sabrosito = {
+    marca: "Sabrosito",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//pedigree
+let pedigree = {
+    marca: "Pedigree",
+    animal: "Perro",
+    edad: "",
+    tamano: "",
+
+}
+
+//sieger
+let sieger = {
+    marca: "Sieger",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//whiskas
+let whiskas = {
+    marca: "Whiskas",
+    animal: "Gato",
+    edad: "",
+    tamano: "",
+
+}
+
+//gati
+let gati = {
+    marca: "Gati",
+    animal: "Gato",
+    edad: "",
+    tamano: "",
+
+}
+
+//kongo
+let kongo = {
+    marca: "Kongo",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
+
+//otros
+let otros = {
+    marca: "Otros",
+    animal: "",
+    edad: "",
+    tamano: "",
+
+}
