f80f7acc3626385431595d1f610be1110ad57444
Alejandro <alejandrodanielmaure@gmail.com>
2021-03-29 20:47:30 -0300

    corregido

--- Proyecto Simulador Interactivo/src/js/script.js
+++ Proyecto Simulador Interactivo/src/js/script.js
@@ -1,102 +1,49 @@
+//constantes
 
+const formulario = document.getElementById("form-group");
+const nombre = formulario.nombre;
+const tipo = formulario.tipo;
+const edad = formulario.edad;
+const tamanio = formulario.tamanio;
+const peso = formulario.peso;
+const marca = formulario.marca;
+const datosResultado = document.getElementById("resultado");
 
-//ingresar
-let mascotaIngresada = '';
-
-
-
-
-//funciones
-
-    
-
-    let datosNombre = document.getElementById("datosNombre").value;
-    let datosPeso = document.getElementById("datosPeso").value;
-    let datosMarca = document.getElementById("datosMarca").value;
-
-    let guardarBtn = document.getElementById("guardar");
-
-    let datosTipo = "";
-
-    let tipoPerro = document.getElementById("perro");
-    let tipoGato = document.getElementById("gato");
-    if (tipoPerro.checked){
-        datosTipo = tipoPerro.value;
-    }else{
-        datosTipo = tipoGato.value;
-    }
-
-
-    let datosEdad = "";
-
-    let edadCachorro = document.getElementById("cachorro");
-    let edadAdulto = document.getElementById("adulto");
-
-    if (edadCachorro.checked){
-        datosEdad = edadCachorro.value;
-    }else{
-        datosEdad = edadAdulto.value;
-    }
-
-
-    let datosTamano = "";
-
-    let tamanoChico = document.getElementById("chico");
-    let tamanoMediano = document.getElementById("mediano");
-    let tamanoGrande = docuemnt.getElementById("grande");
-
-    if (tamanoChico.checked){
-        datosTamano = tamanoChico.value;
-    }else if (tamanoMediano.checked){
-        datosTamano = tamanoMediano.value;
-    }else{
-        datosTamano = tamanoGrande.value;
-    }
-
-    guardarBtn.addEventListener("click", mostrarResultado);
-    document.getElementById("guardar").addEventListener("click", (event) => {event.preventDefault()});
-
-    let mostrarResultado = `<p>Nombre: ${datosNombre}</p><br> <p>Tipo: ${datosTipo}</p><br> <p>Edad: ${datosEdad}</p><br> <p>Tamano: ${datosTamano}</p><br> <p>Peso: ${datosPeso}Kg.</p><br>`
-    
-
-let datosResultado = document.createElement("div");
-
-datosResultado.innerHTML = mostrarResultado;
-
-document.getElementById("petData").appendChild(datosResultado);
-
-
-
-//local storage
-/*let stgNombre = localStorage.setItem('nombre', datosNombre);
-let stgTipo = localStorage.setItem('tipo', datosTipo);
-let stgEdad = localStorage.setItem('edad', datosEdad);
-let stgTamano = localStorage.setItem('tamano', datosTamano);
-let stgPeso = localStorage.setItem('peso', datosPeso);
-let stgMarca = localStorage.setItem('marca', datosMarca);
-*/
 
 //clases
+//creacion del objeto mascota
 class Mascota{
-    constructor(nombre, tipo, edad, tamano, peso, marca) {
+    constructor(nombre, tipo, edad, tamanio, peso, marca){
         this.nombre = nombre;
         this.tipo = tipo;
         this.edad = edad;
-        this.tamano = tamano;
+        this.tamanio = tamanio;
         this.peso = peso;
         this.marca = marca;
     }
     cantidadDeComida(){
-        return this.peso*10*3;
+        return this.peso * 10 * 3;
     }
 }
 
+formulario.addEventListener('submit', (e =>{
+    e.preventDefault();
+    const mascotaAMostrar = new Mascota(nombre.value, tipo.value, edad.value, tamanio.value, peso.value, marca.value);
 
+let mostrarResultado = `
+<p>Nombre: ${mascotaAMostrar.nombre}</p><br> 
+<p>Tipo: ${mascotaAMostrar.tipo}</p><br> 
+<p>Edad: ${mascotaAMostrar.edad}</p><br> 
+<p>Tamano: ${mascotaAMostrar.tamanio}</p><br> 
+<p>Peso: ${mascotaAMostrar.peso}Kg.</p><br>
+<p>Marca de Alimento: ${mascotaAMostrar.marca}</p><br>
+<p>Cantidad de Alimento: ${mascotaAMostrar.cantidadDeComida()} gr.</p><br>
+<button type="submit"  class="save-btn" id="save"> Guardar Mascota </button>
+<button type="submit"  class="input-button" id="clear">Limpiar Busqueda</button>
+`;
 
-        
-mascota1 = new Mascota(nombre, tipo, edad, tamano, peso, marca);
+datosResultado.innerHTML = mostrarResultado;
 
+formulario.reset();
 
-const mascotas = [];
-mascotas.push(new Mascota(nombre, tipo, edad, tamano, peso, marca));
-
+}))
