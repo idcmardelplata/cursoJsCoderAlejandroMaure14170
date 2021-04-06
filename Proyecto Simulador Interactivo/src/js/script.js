33b1f964627bb28a0d4f8d60b15cbc522f1870d1
Alejandro <alejandrodanielmaure@gmail.com>
2021-04-06 18:44:06 -0300

    clase 12 subido

--- Proyecto Simulador Interactivo/src/js/script.js
+++ Proyecto Simulador Interactivo/src/js/script.js
@@ -1,6 +1,6 @@
 //constantes
 
-const formulario = document.getElementById("form-group");
+const formulario = document.getElementById("formGroup");
 const nombre = formulario.nombre;
 const tipo = formulario.tipo;
 const edad = formulario.edad;
@@ -9,51 +9,52 @@ const peso = formulario.peso;
 const marca = formulario.marca;
 const datosResultado = document.getElementById("resultado");
 const datosGuardado = document.getElementById("petData");
-
-
-
+const cantidad = function cantidadDeComida(peso) {var resultado  = peso * 10 * 3;{return resultado}} 
 //clases
 //creacion del objeto mascota
 class Mascota{
-    constructor(nombre, tipo, edad, tamanio, peso, marca){
+    constructor(nombre, tipo, edad, tamanio, peso, marca, cantidad){
         this.nombre = nombre;
         this.tipo = tipo;
         this.edad = edad;
         this.tamanio = tamanio;
         this.peso = peso;
         this.marca = marca;
+        this.cantidad = cantidad(peso);
     }
-    cantidadDeComida(){
-        return this.peso * 10 * 3;
-    }
+    
 }
-
-//funciones
-//calculo cantidad de comida
-
 
 //respuesta a la consulta
 formulario.addEventListener('submit', (e =>{
     e.preventDefault();
-    const mascotaAMostrar = new Mascota(nombre.value, tipo.value, edad.value, tamanio.value, peso.value, marca.value);
+    const mascotaAMostrar = new Mascota(nombre.value, tipo.value, edad.value, tamanio.value, peso.value, marca.value, cantidad);
 
 let mostrarResultado = `
-<p>Nombre: ${mascotaAMostrar.nombre}</p><br> 
-<p>Tipo: ${mascotaAMostrar.tipo}</p><br> 
-<p>Edad: ${mascotaAMostrar.edad}</p><br> 
-<p>Tamano: ${mascotaAMostrar.tamanio}</p><br> 
-<p>Peso: ${mascotaAMostrar.peso}Kg.</p><br>
-<p>Marca de Alimento: ${mascotaAMostrar.marca}</p><br>
-<p>Cantidad de Alimento: ${mascotaAMostrar.cantidadDeComida()} gr.</p><br>
-<button class="save-btn" id="agregar"> Guardar Mascota </button>
-<button class="input-button" id="clear">Limpiar Busqueda</button>
+    <div class="result-container">
+        <div class="result-header">
+            <h3>Resultado de la Busqueda</h3>
+        </div>
+
+        <div class="result-info">
+            <h3>${mascotaAMostrar.nombre}</h3> 
+            <p>Tipo: ${mascotaAMostrar.tipo}</p> 
+            <p>Edad: ${mascotaAMostrar.edad}</p>
+            <p>Tamano: ${mascotaAMostrar.tamanio}</p>
+            <p>Peso: ${mascotaAMostrar.peso}Kg.</p>
+            <p>Marca de Alimento: ${mascotaAMostrar.marca}</p>
+            <p>Cantidad de Alimento: ${mascotaAMostrar.cantidad} gr.</p>
+            <button class="save-btn" id="agregar"> Guardar Mascota </button>
+            <button class="input-button" id="clear">Limpiar Busqueda</button>
+        </div>
+    </div>
 `;
 
 datosResultado.innerHTML = mostrarResultado;
 
 formulario.reset();
 
-document.getElementById('agregar').addEventListener('click', () =>{agregar()
+$('#agregar').click(() =>{agregar()
 
 function agregar(){
     const agregarMascota = localStorage.setItem(mascotaAMostrar.nombre, JSON.stringify(mascotaAMostrar));
@@ -61,29 +62,26 @@ function agregar(){
     let mostrarMascota = JSON.parse(localStorage.getItem(mascotaAMostrar.nombre));
 
     let mostrarMascotaGuardada = `
-<p>Nombre: ${mostrarMascota.nombre}</p><br> 
-<p>Tipo: ${mostrarMascota.tipo}</p><br> 
-<p>Edad: ${mostrarMascota.edad}</p><br> 
-<p>Tamano: ${mostrarMascota.tamanio}</p><br> 
-<p>Peso: ${mostrarMascota.peso}Kg.</p><br>
-<p>Marca de Alimento: ${mostrarMascota.marca}</p><br>
+        <h3>${mostrarMascota.nombre}</h3>
+
+    <p>Tipo: ${mostrarMascota.tipo}</p>
+    <p>Edad: ${mostrarMascota.edad}</p>
+    <p>Tamano: ${mostrarMascota.tamanio}</p>
+    <p>Peso: ${mostrarMascota.peso}Kg.</p>
+    <p>Marca de Alimento: ${mostrarMascota.marca}</p>
+    <p>Cantidad de Alimento; ${mostrarMascota.cantidad} gr.</p>
+    <button id="eliminar"><i class="fas fa-trash-alt"></i></button>
 `;
-    
-    
-    
+  
     datosGuardado.innerHTML = mostrarMascotaGuardada;
     
 }});
 
-document.getElementById('clear').addEventListener('click', () =>{limpiar()
+$('#clear').click(() =>{limpiar()
     
     function limpiar(){
         datosResultado.innerHTML = "";
     }
-    
 })
-
-
-
-}))
+}));
 
