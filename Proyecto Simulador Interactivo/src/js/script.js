c2ffc33b74bba586553c6132ff2977618e253364
Alejandro Maure <alejandrodanielmaure@gmail.com>
2021-05-03 12:10:40 -0300

    en desarrollo

--- Proyecto Simulador Interactivo/src/js/script.js
+++ Proyecto Simulador Interactivo/src/js/script.js
@@ -11,10 +11,8 @@ const marca = formulario.marca;
 const datosResultado = document.getElementById("resultado");
 const datosGuardado = document.getElementById("petData");
 const fotos = document.getElementById("photos");
-<<<<<<< HEAD
 const listaResponsive = document.getElementById("seccionLista");
-=======
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
+
 
 //funciones de calculo de comida
 function cantidadTipoEdad(tipo, edad) {
@@ -205,7 +203,6 @@ class Mascota{
     
 }
 
-<<<<<<< HEAD
 //funcion que calula la cantidad de comida segun los valores
 function cantidad () {
     return Math.round(cantidadTipoEdad(tipo.value, edad.value) * cantidadTamanio(tamanio.value) * cantidadMarca(marca.value) * peso.value);
@@ -213,8 +210,6 @@ function cantidad () {
 
 //constante
 let cantidadComida = cantidad(tipo, edad, tamanio, marca, peso);
-=======
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
 
 
 //respuesta a la consulta
@@ -222,7 +217,6 @@ let cantidadComida = cantidad(tipo, edad, tamanio, marca, peso);
 formulario.addEventListener('submit', (e =>{
     
     e.preventDefault();
-<<<<<<< HEAD
     validarFormulario();
     mostrar = new Mascota(nombre.value, tipo.value, edad.value, tamanio.value, peso.value, marca.value, cantidad(tipo, edad, tamanio, marca, peso));
 
@@ -235,36 +229,15 @@ let mostrarResultado = `
             <p>Peso: ${mostrar.peso} Kg.</p>
             <p>Marca de Alimento: ${mostrar.marca}</p>
             <p>Cantidad de Alimento: ${cantidad(tipo, edad, tamanio, marca, peso)} gr.</p>
-=======
-    
-    const cantidad = Math.round(cantidadTipoEdad(tipo.value, edad.value) * cantidadTamanio(tamanio.value) * cantidadMarca(marca.value) * peso.value);
-
-    const mascotaAMostrar = new Mascota(nombre.value, tipo.value, edad.value, tamanio.value, peso.value, marca.value, cantidad);
-
-let mostrarResultado = `
-        <div id="mascotaAMostrar">
-            <h3>${mascotaAMostrar.nombre}</h3> 
-            <p>Tipo: ${mascotaAMostrar.tipo}</p> 
-            <p>Edad: ${mascotaAMostrar.edad}</p>
-            <p>Tamano: ${mascotaAMostrar.tamanio}</p>
-            <p>Peso: ${mascotaAMostrar.peso}Kg.</p>
-            <p>Marca de Alimento: ${mascotaAMostrar.marca}</p>
-            <p>Cantidad de Alimento: ${mascotaAMostrar.cantidad} gr.</p>
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
             <button class="save-btn" id="agregar"> Guardar Mascota </button>
             <button class="input-button" id="clear">Limpiar Busqueda</button>
         </div>
             `;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
             datosResultado.innerHTML = mostrarResultado;
 
 formulario.reset();
 
-<<<<<<< HEAD
 $('#agregar').click(() =>{
     localStorage.setItem(mostrar.nombre, JSON.stringify(mostrar));
 });
@@ -275,90 +248,29 @@ $('#agregar').click(() =>{
 function mostrarEnDom() {
 
     let mostrarMascota = JSON.parse(localStorage.getItem(mostrar.nombre));
+
         $('#petData').append(`
         <div>
-        <h3>${mostrar.nombre}</h3>
-        <p>Tipo: ${mostrar.tipo}</p>
-        <p>Edad: ${mostrar.edad}</p>
-        <p>Tamano: ${mostrar.tamanio}</p>
-        <p>Peso: ${mostrar.peso}Kg.</p>
-        <p>Marca de Alimento: ${mostrar.marca}</p>
-        <p>Cantidad de Alimento: ${mostrar.cantidadComida} gr.</p>
+        <h3>${mostrarMascota.nombre}</h3>
+        <p>Tipo: ${mostrarMascota.tipo}</p>
+        <p>Edad: ${mostrarMascota.edad}</p>
+        <p>Tamano: ${mostrarMascota.tamanio}</p>
+        <p>Peso: ${mostrarMascota.peso}Kg.</p>
+        <p>Marca de Alimento: ${mostrarMascota.marca}</p>
+        <p>Cantidad de Alimento: ${mostrarMascota.cantidadComida} gr.</p>
         <button id="eliminar"><i class="fas fa-trash-alt"></i></button>
         </div>
     `)}
 
 //evento eliminacion de la mascota guardada y del localstorage
-=======
-
-
-
-//agregado de la mascota a la seccion de mascota guardada y local storage
-//tambien en la funcion muestra en el dom la misma
-$('#agregar').click(() =>{
-    agregar();
-
-function agregar() {
-    $('#agregar').fadeIn("slow", function (){
-        const agregarMascota = localStorage.setItem(mascotaAMostrar.nombre, JSON.stringify(mascotaAMostrar));
-    
-        let mostrarMascota = JSON.parse(localStorage.getItem(mascotaAMostrar.nombre));
-
-            $('#petData').append(`
-            <div>
-            <h3>${mostrarMascota.nombre}</h3>
-            <p>Tipo: ${mostrarMascota.tipo}</p>
-            <p>Edad: ${mostrarMascota.edad}</p>
-            <p>Tamano: ${mostrarMascota.tamanio}</p>
-            <p>Peso: ${mostrarMascota.peso}Kg.</p>
-            <p>Marca de Alimento: ${mostrarMascota.marca}</p>
-            <p>Cantidad de Alimento; ${mostrarMascota.cantidad} gr.</p>
-            <button id="eliminar"><i class="fas fa-trash-alt"></i></button>
-            </div>
-        `)
-
-        document.body.onload = mostrarEnDom;
-        
-        function mostrarEnDom() {
-            if (localStorage.getItem(mostrarMascota.nombre)) {
-                $('#petData').append(`
-                <div>
-                <h3>${mostrarMascota.nombre}</h3>
-                <p>Tipo: ${mostrarMascota.tipo}</p>
-                <p>Edad: ${mostrarMascota.edad}</p>
-                <p>Tamano: ${mostrarMascota.tamanio}</p>
-                <p>Peso: ${mostrarMascota.peso}Kg.</p>
-                <p>Marca de Alimento: ${mostrarMascota.marca}</p>
-                <p>Cantidad de Alimento; ${mostrarMascota.cantidad} gr.</p>
-                <button id="eliminar"><i class="fas fa-trash-alt"></i></button>
-                </div>
-            `)
-
-            }
-
-
-        }
-
-    //evento eliminacion de la mascota guardada y del localstorage
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
     $('#eliminar').click(() =>{
     $('#petData').fadeOut("slow", function eliminar() {
         $('#petData').remove();
         location.reload();
     });
-<<<<<<< HEAD
     localStorage.removeItem(mostrar.nombre);
     })
 
-=======
-    localStorage.removeItem(mascotaAMostrar.nombre);
-    })
-    
-})
-}
-
-});
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
 
 //evento limpieza de resultado de la busqueda
 $('#clear').click(() =>{
@@ -366,7 +278,6 @@ $('#clear').click(() =>{
         datosResultado.innerHTML = ""
         location.reload();
     });
-<<<<<<< HEAD
 })
 
 //funcion que valida el formulario si no se selecciona una marca
@@ -379,7 +290,7 @@ function validarFormulario(){
 }
 
 //carga de la mascota guardada al recargar o abrir la pagina
-window.onload = mostrarEnDom();
+// window.onload = mostrarEnDom();
 
 
 $("#lista").click(() => {
@@ -391,20 +302,12 @@ $("#lista").click(() => {
     closeList();
 })
 
-        //MANEJO DE THE CAT API Y THE DOG API
-=======
-}
-  
 
-)
-}));
-
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
+//MANEJO DE THE CAT API Y THE DOG API
 
 //carga de fotos random de perros
 $("#dp").click(() => {
     $.get('https://dog.ceo/api/breeds/image/random', (res) => {
-<<<<<<< HEAD
     listaResponsive.innerHTML = "";
     fotos.innerHTML = `
     <div class="box-api">
@@ -412,18 +315,12 @@ $("#dp").click(() => {
     <img src="${res.message}">
     </div>`
     closePic();
-=======
-    console.log()
-    
-    fotos.innerHTML = `<img src="${res.message}" width=300px height=300px>`
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
 });
 });
 
 //carga de fotos random de gatos
 $("#cp").click(() => {
     $.get(' https://api.thecatapi.com/v1/images/search', (res) => {
-<<<<<<< HEAD
     listaResponsive.innerHTML = "";
     fotos.innerHTML = `
     <div class="box-api">
@@ -452,10 +349,3 @@ function closeList() {
         })
     })
 }
-=======
-    console.log(res)
-    
-    fotos.innerHTML = `<img src="${res[0].url}" width=300px height=300px>`
-});
-});
->>>>>>> f1881ce83a5b0b5f09f0ceed5e385fb841c8bf8f
